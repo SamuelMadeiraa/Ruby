a5f7b71386b2e879bb2461e66192a278ba7560bf
SamuelMadeiraa <samuelmadeiradev@gmail.com>
2023-11-01 11:16:53 -0300

    Numero Maior

---
+++ maior.rb
@@ -0,0 +1,10 @@
+print "Digite um numero:"
+numero = gets.chomp.to_i
+print "Digite outro numero:"
+numero2 = gets.chomp.to_i
+
+if numero > numero2
+puts "O numero maior é: #{numero}"
+else
+puts "O numero maior é: #{numero2}"
+end
